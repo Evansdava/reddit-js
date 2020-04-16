ee53b112464419fb9fdd902ad2e8634c24cef7a7
David Evans <evansdav.a@gmail.com>
2020-04-16 14:47:48 -0700

    Users can see individual posts

--- controllers/posts.js
+++ controllers/posts.js
@@ -26,4 +26,16 @@ module.exports = (app) => {
         console.log(err.message)
       })
   })
+
+  // GET SINGLE POST
+  app.get('/posts/:id', function (req, res) {
+    // LOOK UP THE POST
+    Post.findById(req.params.id).lean()
+      .then(post => {
+        res.render('posts-show', { post })
+      })
+      .catch(err => {
+        console.log(err.message)
+      })
+  })
 }
