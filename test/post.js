b5259388cf487ffaf82eaaf31bd312982d4f0662
David Evans <evansdav.a@gmail.com>
2020-05-05 11:37:19 -0700

    Fixed posts test

--- test/post.js
+++ test/post.js
@@ -6,12 +6,15 @@ const chaiHttp = require('chai-http')
 const expect = chai.expect
 const describe = mocha.describe
 const it = mocha.it
+const before = mocha.before
 const after = mocha.after
 
 // Import the Post model from our models folder so we
 // we can use it in our tests.
 const Post = require('../models/post')
+const User = require('../models/user')
 const server = require('../server')
+const agent = chai.request.agent(app)
 
 chai.should()
 chai.use(chaiHttp)
@@ -24,6 +27,23 @@ describe('Posts', function () {
     url: 'https://www.google.com',
     summary: 'post summary'
   }
+  const user = {
+    username: 'poststest',
+    password: 'testposts'
+  }
+  before(function (done) {
+    agent
+      .post('/sign-up')
+      .set('content-type', 'application/x-www-form-urlencoded')
+      .send(user)
+      .then(function (res) {
+        done()
+      })
+      .catch(function (err) {
+        done(err)
+      })
+  })
+
   it('Should create with valid attributes at POST /posts/new', function (done) {
     // Checks how many posts there are now
     Post.estimatedDocumentCount()
@@ -58,7 +78,23 @@ describe('Posts', function () {
       })
   })
 
-  after(function () {
-    Post.findOneAndDelete(newPost)
+  after(function (done) {
+    Post.findOneAndDelete(newPost).lean()
+      .then(function (res) {
+        agent.close()
+
+        User.findOneAndDelete({
+          username: user.username
+        })
+          .then(function (res) {
+            done()
+          })
+          .catch(function (err) {
+            done(err)
+          })
+      })
+      .catch(function (err) {
+        done(err)
+      })
   })
 })
