d018159f0c570dc1fa01be5d0374e0a0e68a93bd
David Evans <evansdav.a@gmail.com>
2020-05-05 10:31:30 -0700

    Users can sign up

--- server.js
+++ server.js
@@ -3,12 +3,17 @@ const app = express()
 const port = 3000
 const bodyParser = require('body-parser')
 const expressValidator = require('express-validator')
+require('dotenv').config()
 
 // Set db
 require('./data/reddit-db')
 
 // Middleware
 const exphbs = require('express-handlebars')
+var cookieParser = require('cookie-parser')
+// const jwt = require('jsonwebtoken')
+
+app.use(cookieParser()) // Add this after you initialize express.
 
 // Use Body Parser
 app.use(bodyParser.json())
@@ -27,6 +32,8 @@ app.get('/posts/new', (req, res) => res.render('posts-new'))
 require('./controllers/posts.js')(app)
 
 require('./controllers/comments.js')(app)
+
+require('./controllers/auth.js')(app)
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
