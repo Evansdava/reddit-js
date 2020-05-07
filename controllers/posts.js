50e70390124b4826f969470e5fe4f02c4b0e55ed
David Evans <evansdav.a@gmail.com>
2020-05-07 11:03:44 -0700

    Refactored populate code

--- controllers/posts.js
+++ controllers/posts.js
@@ -44,9 +44,7 @@ module.exports = (app) => {
   // GET SINGLE POST
   app.get('/posts/:id', function (req, res) {
     var currentUser = req.user
-    // LOOK UP THE POST
-
-    Post.findById(req.params.id).populate({ path: 'comments', populate: { path: 'author' } }).populate('author')
+    Post.findById(req.params.id).populate('comments').lean()
       .then(post => {
         res.render('posts-show', { post, currentUser })
       })
@@ -58,7 +56,7 @@ module.exports = (app) => {
   // SUBREDDIT
   app.get('/n/:subreddit', function (req, res) {
     var currentUser = req.user
-    Post.find({ subreddit: req.params.subreddit }).populate('author')
+    Post.find({ subreddit: req.params.subreddit }).lean()
       .then(posts => {
         res.render('posts-index', { posts, currentUser })
       })
